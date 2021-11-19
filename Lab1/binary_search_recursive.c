b93ed14c2b0572f2d8c9b7629493a321740c5bda
ananya0809 <ananya.199303010@muj.manipal.edu>
2021-11-19 23:44:55 +0530

    modified code of binary search using recursion for unsorted array

--- Lab1/binary_search_recursive.c
+++ Lab1/binary_search_recursive.c
@@ -2,19 +2,30 @@
 //199303010
 
 #include<stdio.h>
-#include<stdlib.h>
 int binary_search(int a[],int low, int high, int key)
 {
 	if(low > high)
 	{
-		
+		return -1;
+	}
+	else if ( low <= high)
+	{
+		int mid = low + (high - low)/2;
+		if(a[mid] == key)
+		{
+			return mid;
+		}
+		if (a[mid] > key)
+		{
+			return binary_search(a,low,mid-1,key);
+		}
+		return binary_search(a,mid+1,high,key);
 	}
 }
-return -1;	
-}
+
 int main()
 {
-	int i,n,key;
+	int i,j,n,p,key;
 	printf("enter value of n\n");
 	scanf("%d",&n);
 	int A[n];
@@ -24,12 +35,30 @@ int main()
 	{
 		scanf("%d",&A[i]);
 	}
+	for (j = 1; j<= n-1; j++)
+	{
+		i = j-1;
+		key = A[j];
+	
+	while (i>=0 && A[i]>=key)
+	{
+		A[i+1] = A[i];
+		i = i-1;
+		
+	}
+	A[i+1] = key;
+	}
+	printf("sorted array\n");
+	for(p = 0; p <= n-1; p++)
+	{
+		printf("%d\n",A[p]);
+	}
 	printf("Enter a key to search: ");
 	scanf("%d",&key);
-	int search = binary_search(A,n,key);
+	int search = binary_search(A,0,n-1,key);
 	if (search != -1)
 	{
-		printf("Element found at %d location", search);
+		printf("Element found at %d index", search);
 	}
 	else {
 		printf("Element not in array");
