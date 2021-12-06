777f9bafa3fedb6d2f8d377eabc2eb497896b802
ananya0809 <ananya.199303010@muj.manipal.edu>
2021-12-07 03:11:24 +0530

    added code for Depth First Search

--- Lab 7/DFS.c
+++ Lab 7/DFS.c
@@ -1,14 +1,46 @@
+//Ananya Agrawal
+//199303010
+
 #include<stdio.h>
-# define V 5
+#define V 20
+
+int Graph[V][V], completed[V], n;
+int DFS(int i)
+{
+    int j;
+	printf("\n %d", i);
+    completed[i] = 1;
+    for(j = 0; j < n; j++)
+    {
+	    if(!completed[j] && Graph[i][j] == 1)
+        {
+            DFS(j);
+        }
+    }
+    return 0;
+}
+ 
 int main()
 {
-	int i,j;
-	int Graph[V][V]{
-	{ 
+	int i, j;
+	printf("Enter the number of vertices:\n");
+	scanf("%d",&n);
+	printf("Enter an adjacency matrix: \n");
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+		{
+			scanf("%d",&Graph[i][j]);
+		}
+		
 	}
-	
-	
-	
-	
-	}
+   for(i = 0; i < n; i++)
+	{
+    	completed[i]=0;
+    }
+    printf("Depth First Search is\n");
+ 
+    DFS(0);
+ 
+    return 0;
 }
