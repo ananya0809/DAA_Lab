65b9c85a8601110aed31a19637c4cffa2fc95db4
ananya0809 <ananya.199303010@muj.manipal.edu>
2021-12-06 18:53:35 +0530

    corrected code for BFS traversal using array

--- Lab 7/BFS.c
+++ Lab 7/BFS.c
@@ -1,39 +1,61 @@
+//Ananya Agrawal
+//199303010
+
 #include<stdio.h>
-#include<conio.h>
-int a[20][20],q[20],visited[20],n,i,j,f=0,r=-1;
-void bfs(int v) {
-	for (i=1;i<=n;i++)
-	  if(a[v][i] && !visited[i])
-	   q[++r]=i;
-	if(f<=r) {
-		visited[q[f]]=1;
-		bfs(q[f++]);
+#define V 20
+
+int Graph[V][V], q[V], visited[V], n, i, j, f = 0, r = -1;
+
+void BFS(int v)
+{
+	for(i = 1; i <= n; i++)
+	{
+		if(Graph[v][i] && !visited[i])
+		{
+			q[++r] = i;
+		}
+	}
+	if(f <= r)
+	{
+		visited[q[f]] = 1;
+		BFS(q[f++]);
 	}
 }
-void main() {
+
+int main()
+{
 	int v;
-//	clrscr();
-	printf("\n Enter the number of vertices:");
+	printf("Enter the number of vertices:\n");
 	scanf("%d",&n);
-	for (i=1;i<=n;i++) {
-		q[i]=0;
-		visited[i]=0;
+	for(i = 1; i <= n; i++)
+	{
+		q[i] = 0;
+		visited[i] = 0;
 	}
-	printf("\n Enter graph data in matrix form:\n");
-	for (i=1;i<=n;i++)
-	  for (j=1;j<=n;j++)
-	   scanf("%d",&a[i][j]);
-	printf("\n Enter the starting vertex:");
+	printf("Enter an adjacency matrix: \n");
+	for(i = 1; i <= n; i++)
+	{
+		for(j = 1; j <= n; j++)
+		{
+			scanf("%d",&Graph[i][j]);
+		}
+		
+	}
+	printf("Enter the starting vertex: \n");
 	scanf("%d",&v);
-	bfs(v);
-	printf("\n The node which are reachable are:\n");
-	for (i=1;i<=n;i++)
-	  if(visited[i])
-	   printf("%d\t",i); else
-	   printf("\n Bfs is not possible");
-	getch();
+	BFS(v);
+	printf("The nodes that will be covered are:\n");
+	for(i = 1; i <= n; i++)
+	{
+		if(visited[i])
+		{
+			printf("%d\t", i);
+		}
+		else
+		{
+			printf("Node unreachable");
+			break;
+		}
+	}
 }
 
-
-//binomial coeff & fibonacchio
-// check phone reminders
