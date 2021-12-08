c7f50ddf7c326f234f6815fd9c5e29582fa19a1e
ananya0809 <ananya.199303010@muj.manipal.edu>
2021-12-08 09:20:58 +0530

    corrected code for the knapsack problem using Greedy approach

--- Lab 5/knapsack_greedy.c
+++ Lab 5/knapsack_greedy.c
@@ -2,44 +2,62 @@
 //199303010
 
 #include<stdio.h>
-int maximum(float A[],int n)
-{
-	int i;
-	float max = A[0];
-	for(i=0;i<n;i++)
-	{
-		if(max < A[i])
-		{
-			max = A[i];
-		}
-	}
-	
-	printf("maximum ratio from array = %.2f",max);
-	return i;
-}
-int main()
-{
-	int m;
-	printf("enter weight of knapsack: ");
-	scanf("%d",&m);
-	int n;
-	printf("enter number of items: ");
-	scanf("%d",&n);
-	int w[n],p[n],i;
-	
-	for (i=0;i<n;i++)
-	{
-		printf("enter the weight of item %d: ",i+1);
-		scanf("%d",&w[i]);
-		printf("enter the profit of item %d: ",i+1);
-		scanf("%d",&p[i]);
-	}
-	float r[n];
-	for (i=0;i<n;i++)
-	{
-		r[i] = ((float)p[i]/(float)w[i]);
-		printf("the profit by weight ratio of item %d = %.2f\n", i+1, r[i]);
-	}
-	maximum(r,n);
-}
 
+int main() {
+    int i;
+    int n;
+    printf("\nEnter number of items: ");
+    scanf("%d", &n);
+    
+    int size;
+    printf("\nEnter knapsack limit: ");
+    scanf("%d", &size);
+
+    printf("\nEnter profits: ");
+    int profits[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &profits[i]);
+    }
+
+    printf("\nEnter weights: ");
+    int wts[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &wts[i]);
+    }
+    float rats[n];
+    for (i = 0; i < n; i++) {
+        rats[i] = profits[i]/wts[i];
+    }
+
+    float prof = 0;
+    float items[n];
+    for (i = 0; i < n; i++) {
+        items[i] = 0;
+    }
+
+    while(size > 0) {
+        int max = 0;
+        float rat = rats[max];
+        for (i = 1; i < n; i++) {
+            float t_rat = rats[i];
+            if (rat < t_rat) {
+                max = i;
+                rat = t_rat;
+            }
+        }
+        int t_wt = wts[max];
+        if (t_wt <= size) {
+            items[max] = 1;
+            rats[max] = -1;
+            prof = prof + profits[max];
+        }
+        else {
+            items[max] = (float)size/t_wt;
+        }
+        size = size - t_wt;
+    }
+
+    printf("\n");
+
+    printf("\nFinal profit: %.2f", prof);
+}
